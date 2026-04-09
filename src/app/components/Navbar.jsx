53b7a63143e71936101756306036470918e18ca6
resty-mukasa <restyseanice@gmail.com>
2026-04-09 14:41:26 +0300

    Added and updated logo in navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,19 +1,21 @@
-/* eslint-disable @next/next/no-img-element */
-import Link from "next/link";
+ import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-5 bg-white shadow-md sticky top-0 z-50">
 
       {/* Logo */}
-      <Link href="/" className="flex items-center space-x-2">
-        <img
-          src="/images/logo.png"
-          alt="Blessed Hands Home Care"
-          className="h-10 w-auto"
+      <Link href="/" className="flex items-center gap-3">
+        <Image
+          src="/images/Care%20Axis%20Agency%20logo.png"
+          alt="Care Axis Agency logo"
+          width={45}
+          height={45}
+          className="rounded-full shadow-md hover:scale-105 transition"
         />
-        <span className="text-lg font-bold text-blue-600 hidden sm:block">
-         Care Axis Agency
+        <span className="text-lg font-bold text-blue-700 tracking-wide hidden sm:block">
+          Care Axis Agency
         </span>
       </Link>
 
